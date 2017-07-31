dcfba4f7ad22b7f1f8bd12415a7e62e674901fdd
Alfredo <mendoza1@us.ibm.com>
2017-07-31 22:27:34 +0000

    change scripts/apply_h2o_patch.sh to check if patch for h2o/py already applied if not apply the patch

--- scripts/apply_h2o_patch.sh
+++ scripts/apply_h2o_patch.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
 cd /usr/local/lib/python2.7/dist-packages/h2o
-patch < /usr/local/scripts/h2o.py.patch
+
+patch --dry-run < /usr/local/scripts/h2o.py.patch
+if [$? -eq 0]; then
+	patch < /usr/local/scripts/h2o.py.patch
+fi
