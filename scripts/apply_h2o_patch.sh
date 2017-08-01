20b3fcc9ef413d4dbc482d52a8d6d793899297c2
Alfredo <mendoza1@us.ibm.com>
2017-08-01 15:03:10 +0000

    h2o.py patch corrected

--- scripts/apply_h2o_patch.sh
+++ scripts/apply_h2o_patch.sh
@@ -3,6 +3,6 @@
 cd /usr/local/lib/python2.7/dist-packages/h2o
 
 patch --dry-run < /usr/local/scripts/h2o.py.patch
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
 	patch < /usr/local/scripts/h2o.py.patch
 fi
