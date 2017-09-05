756e1540afa16f05297929d4939e8e9cd84d312e
Alfredo <mendoza1@us.ibm.com>
2017-09-05 19:39:07 +0000

    modify Dockerfile to include supervisor and mapd

---
+++ scripts/startmapd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/jvm/java-8-openjdk-ppc64el/jre/lib/ppc64le/server
+export DISPLAY=:1
+
+X :1 &
+/usr/bin/nvidia-xconfig --use-display-device=none --enable-all-gpus --preserve-busid
+cd /home/MapD-full/mapd-ee-3.1.1-20170628-45a6fa8-Linux-ppc64le-render-glx
+./startmapd 2>&1 | tee /tmp/mapd.log
