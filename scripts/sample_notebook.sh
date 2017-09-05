c693c61a0dbf2e57d96fa5c1b402d5298fda0939
Alfredo <mendoza1@us.ibm.com>
2017-09-05 20:25:26 +0000

    add start ssh

--- scripts/sample_notebook.sh
+++ scripts/sample_notebook.sh
@@ -4,5 +4,6 @@ if [ ! -d /data/demo ]; then
     cp -a /usr/local/samples /data/demo
 fi
 
+sudo /etc/init.d/ssh start
 nohup /usr/local/scripts/runh2o 2>&1 | tee /tmp/h2odeepwater.log &
 exec /usr/local/bin/nimbix_notebook "$@"
