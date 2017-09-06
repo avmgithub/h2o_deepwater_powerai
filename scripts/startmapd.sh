8ef704f7ca376b18c3eaa8a6d8de4b44bb85ea49
Alfredo <mendoza1@us.ibm.com>
2017-09-06 18:55:08 +0000

    corrected script to startmapd

--- scripts/startmapd.sh
+++ scripts/startmapd.sh
@@ -5,5 +5,5 @@ export DISPLAY=:1
 
 X :1 &
 /usr/bin/nvidia-xconfig --use-display-device=none --enable-all-gpus --preserve-busid
-cd /home/MapD-full/mapd-ee-3.1.1-20170628-45a6fa8-Linux-ppc64le-render-glx
+cd /home/mapd-ee-3.1.1-20170628-45a6fa8-Linux-ppc64le-render-glx
 ./startmapd 2>&1 | tee /tmp/mapd.log
